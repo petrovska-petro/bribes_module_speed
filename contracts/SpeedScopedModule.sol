f10af77b2f8edc72a4cf8f3580b2b6955ed4e799
Petrovska <petrovska@badger.finance>
2022-06-20 14:15:40 +0200

    Add comments in code for context facilitation

--- contracts/SpeedScopedModule.sol
+++ contracts/SpeedScopedModule.sol
@@ -61,6 +61,10 @@ contract SpeedScopedModule {
                ADMIN - GOVERNANCE
     ****************************************/
 
+    /// @dev Set whether or not calls can be made to an address.
+    /// @notice Only callable by governance.
+    /// @param target Address to be allowed/disallowed.
+    /// @param allow Bool to allow (true) or disallow (false) calls to target.
     function setTargetAllowed(address target, bool allow)
         external
         onlyGovernance
@@ -69,11 +73,20 @@ contract SpeedScopedModule {
         emit SetTargetAllowed(target, allowedTargets[target].allowed);
     }
 
+    /// @dev Sets whether or not calls to an address should be scoped to specific function signatures.
+    /// @notice Only callable by governance.
+    /// @param target Address to be scoped/unscoped.
+    /// @param scoped Bool to scope (true) or unscope (false) function calls on target.
     function setScoped(address target, bool scoped) public onlyGovernance {
         allowedTargets[target].scoped = scoped;
         emit SetTargetScoped(target, allowedTargets[target].scoped);
     }
 
+    /// @dev Sets whether or not a specific function signature should be allowed on a scoped target.
+    /// @notice Only callable by governance.
+    /// @param target Scoped address on which a function signature should be allowed/disallowed.
+    /// @param functionSig Function signature to be allowed/disallowed.
+    /// @param allow Bool to allow (true) or disallow (false) calls a function signature on target.
     function setAllowedFunction(
         address target,
         bytes4 functionSig,
@@ -87,11 +100,17 @@ contract SpeedScopedModule {
         );
     }
 
+    /// @dev Adds an executor to the Set of allowed addresses.
+    /// @notice Only callable by governance.
+    /// @param _executor Address which will have rights to call `checkTransactionAndExecute`.
     function addExecutor(address _executor) external onlyGovernance {
         require(_executor != address(0), "zero-address!");
         _executors.add(_executor);
     }
 
+    /// @dev Removes an executor to the Set of allowed addresses.
+    /// @notice Only callable by governance.
+    /// @param _executor Address which will not have rights to call `checkTransactionAndExecute`.
     function removeExecutor(address _executor) external onlyGovernance {
         require(_executor != address(0), "zero-address!");
         _executors.remove(_executor);
@@ -101,6 +120,10 @@ contract SpeedScopedModule {
                ADMIN - EXECUTORS
     ****************************************/
 
+    /// @dev Allows executing specific calldata into an address thru a gnosis-safe, which have enable this contract as module.
+    /// @notice Only callable by executors.
+    /// @param to Contract address where we will execute the calldata.
+    /// @param data Calldata to be executed within the boundaries of the `allowedFunctions`.
     function checkTransactionAndExecute(address to, bytes calldata data)
         external
         onlyExecutors
@@ -130,10 +153,14 @@ contract SpeedScopedModule {
                PUBLIC FUNCTION
     ****************************************/
 
+    /// @dev Returns all addresses which have executor role
     function getExecutors() public view returns (address[] memory) {
         return _executors.values();
     }
 
+    /// @dev Returns bool to indicate if a function signature is allowed for a target address.
+    /// @param target Address to check.
+    /// @param functionSig Signature to check.
     function isAllowedFunction(address target, bytes4 functionSig)
         public
         view
